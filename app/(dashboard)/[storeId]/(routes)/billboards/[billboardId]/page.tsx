966990712441497ceb54adb8d9ff368e9d9e4955
fazal7090 <mfazal7090@gmail.com>
2025-07-05 01:28:24 +0500

    Change in ecommerce billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,11 +1,14 @@
 import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
-const BillboardPage = async ({
-  params
-}: {
-  params: { billboardId: string }
-}) => {
+interface BillboardPageProps {
+  params: {
+    billboardId: string;
+  };
+}
+
+const BillboardPage = async ({ params }: BillboardPageProps) => {
+
   const billboard = await prismadb.billboard.findUnique({
     where: {
       id: params.billboardId
